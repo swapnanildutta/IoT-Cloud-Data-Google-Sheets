8860dd59630fa9e36ddbd16bd69d84f5acb493c4
Sandipan Dey <sandipandey.12@gmail.com>
2020-04-16 19:56:42 +0530

    added indentation with Spaces:4

--- wifi.ino
+++ wifi.ino
@@ -1,26 +1,26 @@
 #include "ESP8266WiFi.h"
 
-const char* ssid = "ssid"; //Enter SSID
-const char* password = "password"; //Enter Password
+const char *ssid = "ssid";         //Enter SSID
+const char *password = "password"; //Enter Password
 
 void setup(void)
-  { 
+{
     Serial.begin(9600);
     // Connect to WiFi
-    WiFi.begin(ssid,password);
-    while (WiFi.status() != WL_CONNECTED) 
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
     {
-       delay(1000);
-       Serial.print("*");
+        delay(1000);
+        Serial.print("*");
     }
-    
+
     Serial.println("");
     Serial.println("WiFi connection Successful");
     Serial.print("The IP Address of ESP8266 Module is: ");
-    Serial.print(WiFi.localIP());// Print the IP address
-  }
-  
-  void loop() 
-  {
+    Serial.print(WiFi.localIP()); // Print the IP address
+}
+
+void loop()
+{
     // EMPTY
-  }
+}
